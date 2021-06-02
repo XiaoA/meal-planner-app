419f5a2e929fce57b5df4159f3f8ad1a870dae22
Andrew Buckingham <abuckingham99@gmail.com>
2021-06-02 09:03:50 -0700

    Implement User Flow One: Unauthenticated user can perform recipe search

---
+++ forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField
+from wtforms.validators import InputRequired
+from config import API_KEY
+
+class SearchRecipesForm(FlaskForm):
+    query = StringField("Recipe Search", validators=[InputRequired(message="Search term can't be blank.")])
+    apiKey = API_KEY
+
+    
