22c7fff56b0f498ea63bfddc6047f4867c04fb77
Andrew Buckingham <abuckingham99@gmail.com>
2021-06-06 17:31:43 -0700

    Add basic user list display

--- project/users/routes.py
+++ project/users/routes.py
@@ -1,5 +1,11 @@
 from . import users_blueprint
+from flask import current_app, render_template
+import requests
+from project.models import User
+from project import database
 
 @users_blueprint.route('/users')
-def users_list():
-    return "User List"
+def list_users():
+    users = User.query.order_by(User.id).all()
+    return render_template('users/index.html', users=users)
+
